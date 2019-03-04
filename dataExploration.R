19288ab30854f4c8a533e44ebda84a2bb0eb7002
zakwhich <zakwhich@gmail.com>
2019-03-03 22:10:40 -0600

    Created file for model work. Created logic in file to only use factor levels for pickup_area where number of trips from area is > 0.5% of all (filtered, non NA) observations. Else pickup_area="Other". This will speed up the model calculations.
    Updated readme

--- dataExploration.R
+++
@@ -1,9 +0,0 @@
-library(dplyr)
-library(tidyverse)
-source("R/taxiData.R")
-data1m <- taxiData(100000)
-
-data1mDF <- data.frame(data1m$rawData$fare,data1m$rawData$company)
-colnames(data1mDF) <- c('fare','company')
-
-data1mOrder <- data1mDF[order(data1mDF$fare,decreasing=TRUE),]
